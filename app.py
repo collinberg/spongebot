478f2fd1fc7b7a0d02e6a12cf06be129ec042291
Collin Berg <berg.collin@gmail.com>
2018-03-05 22:00:28 -0500

    Init
    Added v.0.0.1 of the basic spongebot

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import json
+import string
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+  log('Recieved {}'.format(data))
+
+  # We don't want to reply to ourselves!
+  if data['name'] != 'SpongeBob':
+    newName = data['name'].replace(' ', '-')
+    newText = data['text'].replace(' ', '-')
+    msg = 'https://memegen.link/spongebob/{}/{}.jpg'.format(newName, newText)
+    send_message(msg)
+
+  return "ok", 200
+
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
+  
+def log(msg):
+  print(str(msg))
+  sys.stdout.flush()
